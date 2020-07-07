8ddc7979a8c104ff6c877d92941b3e7b9fc6d9d4
mark <markpackham1@gmail.com>
2020-07-07 10:17:56 +0100

    tidy up code

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,14 +1,5 @@
 import React from 'react'
 
-// const Comment = ({children}, {author}) => {
-//   return (
-//     <li className="comment">
-//     <h4>{children}</h4>
-//     <p>{author}</p>
-//   </li>
-//   );
-// }
-
 const Comment = ({children}, {author}) => <li className="comment"><h4>{children}</h4><p>{author}</p></li>
 
 export default Comment;
