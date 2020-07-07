ce290a14d5ecc918b8658b912660bec216908f62
mark <markpackham1@gmail.com>
2020-07-07 10:10:12 +0100

    clean up code

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,24 +1,6 @@
 import React from "react";
 import Comment from "./Comment";
 
-// class CommentList extends Component {
-//   render() {
-//     const commentNodes = this.props.comments.map(comment => {
-//       return (
-//         <Comment author={comment.author} key={comment.id}>
-//           {comment.text}
-//         </Comment>
-//       );
-//     });
-
-//     return (
-//       <ul className="comment-list">
-//         {commentNodes}
-//       </ul>
-//     );
-//   }
-// }
-
 const CommentList = (props) => {
 
   const commentNodes = props.comments.map(comment => {
