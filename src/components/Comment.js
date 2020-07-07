126f55f8fe7b65d8d65c655a1c6f79298326a678
mark <markpackham1@gmail.com>
2020-07-07 10:14:37 +0100

    fit code onto one line

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-const Comment = ({children}, {author}) => {
-  return (
-    <li className="comment">
-    <h4>{children}</h4>
-    <p>{author}</p>
-  </li>
-  );
-}
+// const Comment = ({children}, {author}) => {
+//   return (
+//     <li className="comment">
+//     <h4>{children}</h4>
+//     <p>{author}</p>
+//   </li>
+//   );
+// }
+
+const Comment = ({children}, {author}) => <li className="comment"><h4>{children}</h4><p>{author}</p></li>
 
 export default Comment;
